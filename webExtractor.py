f2d01333c88c1243a9d74646c15b4aa2254559a4
fborsani <fborsani96@gmail.com>
2023-07-04 00:50:20 +0200

    Implemented depth control and refactored code

--- webExtractor.py
+++ webExtractor.py
@@ -7,10 +7,6 @@ from enum import Enum
 from bs4 import BeautifulSoup, Comment, Tag
 from urllib.parse import urlparse
 
-urlsToScan = []
-urlsScanned = []
-urlExclusions = re.compile("^(\.|http|https|#|//).*")
-
 class Settings():
     outputVerbose = 0
     depthLimit = -1
@@ -48,7 +44,7 @@ class Settings():
         else:
             self.outputVerbose = self.VerboseLevels.NONE
 
-        if args["max_depth"]:
+        if args["max_depth"] is not None:
             self.depthLimit = args["max_depth"]
 
         self.outputFile = args["output"]
@@ -136,61 +132,97 @@ class OutputManager():
         if self.fileJson:
             json.dump(self.jsonData,self.fileJson)
 
+class Crawler():
+    maxDepth = -1
+    startUrl = ""
+    domain = ""
 
-def findElements(elementsList,soup):
-    if elementsList:
-        found = []
+    urlsScanned = []
+    urlExclusions = re.compile("^(\.|http|https|#|//).*")
 
-        for target in elementsList:
-            if target == 'comment':
-                found += soup.find_all(string = lambda text: isinstance(text,Comment))
-            if target.find(".") != -1:
-                args = target.split(".")
-                tmpList = soup.find_all(args[0])
-                for item in tmpList:
-                    found.append(item.get(args[1]))
-            else:
-                found += soup.find_all(target)
-        return found
+    parserOut = None
+    parserlinks = None
 
+    def __init__(self, settings:Settings, parser = None):
+        self.startUrl = settings.connectionParams["url"]
+        self.domain = settings.connectionParams["domain"]
+        self.maxDepth = settings.depthLimit
+        self.parserOut = parser
+        self.parserLinks = Parser(settings, ["a"])
 
-def getNextPages(soup,domain,currentUrl):
-    for link in soup.find_all("a"):
-        path = link.get("href")
-        if path and not urlExclusions.match(path):
-                if path.startswith("/"):
-                    path = domain+path
+    def crawl(self):
+        self.parserOut.parse(self.startUrl)
+        self._getPages(self.startUrl,0)
+
+    def _getPages(self, url:str, currentDepth: int):
+        if self.maxDepth >= 0 and currentDepth >= self.maxDepth:
+            return
+        
+        for link in self.parserLinks.parse(url):
+            currUrl = link.get("href")
+            print(url+" - "+str(currentDepth)+"/"+str(self.maxDepth))
+            if currUrl and not self.urlExclusions.match(currUrl) and currUrl not in self.urlsScanned:
+                if currUrl.startswith("/"):
+                    fpath = self.domain+currUrl
                 else:
-                    path = domain+"/"+path
-                urlsToScan.append(path)
-    urlsToScan.pop(0)
-    urlsScanned.append(currentUrl)
+                    fpath = self.domain+"/"+currUrl
+                self.parserOut.parse(fpath)
+                self.urlsScanned.append(currUrl)
+                self._getPages(fpath, currentDepth+1)
+                
+class Parser():
+    s = None
+    om = None
+    match = []
 
+    def __init__(self, s:Settings, match:list=None, om:OutputManager=None):
+        self.match = match
+        self.s = s
+        self.om = om
+
+        if match:
+            self.match = match
+        else:
+            self.match = s.filters
+
+    def parse(self,url:str):
+        elements = []
+        r = requests.get(
+                    url,
+                    verify=self.s.connectionParams["verifyCert"],
+                    headers=self.s.connectionParams["headers"],
+                    cookies=self.s.connectionParams["cookies"],
+                    timeout=self.s.connectionParams["timeout"],
+                    allow_redirects=self.s.connectionParams["followRedirect"]
+                    )
+        if(r.status_code == 200):
+                elements += self.findElements(self.match,r)
+                if self.om:
+                    self.om.printMatches(elements, url)
+        elif self.s.outputVerbose == Settings.VerboseLevels.HIGH:
+            if self.om:
+                self.om.print("The request for url {} returned code {}".format(url,r.status_code))
+        return elements
+  
+    def findElements(self, elementsList:list, r):
+        found = []
+        if elementsList and r:        
+            soup = BeautifulSoup(r.text,"html.parser")
+            for target in elementsList:
+                if target == 'comment':
+                    found += soup.find_all(string = lambda text: isinstance(text,Comment))
+                if target.find(".") != -1:
+                    args = target.split(".")
+                    tmpList = soup.find_all(args[0])
+                    for item in tmpList:
+                        found.append(item.get(args[1]))
+                else:
+                    found += soup.find_all(target)
+            return found
 
 
 if __name__== "__main__":
     s = Settings()
-    om = OutputManager(s)
-
-    domain = s.connectionParams["domain"]
-    urlsToScan.append(s.connectionParams["url"])
-    file = None
-
-    for url in urlsToScan:
-        if url not in urlsScanned:
-            r = requests.get(
-                             url,
-                             verify=s.connectionParams["verifyCert"],
-                             headers=s.connectionParams["headers"],
-                             cookies=s.connectionParams["cookies"],
-                             timeout=s.connectionParams["timeout"],
-                             allow_redirects=s.connectionParams["followRedirect"]
-                             )
-            sleep(s.connectionParams["wait"])
-            if(r.status_code == 200):
-                soup = BeautifulSoup(r.text,"html.parser")
-                om.printMatches(findElements(s.filters,soup),url)
-                getNextPages(soup,domain,url)
-            elif s.outputVerbose == Settings.VerboseLevels.HIGH:
-                om.print("The request for url {} returned code {}".format(url,r.status_code))
-    om.writeJsonFile()
+    c = Crawler(s, Parser(s, om=OutputManager(s)))
+    #c = Crawler(s, Parser(s, om=None))
+    c.crawl()
